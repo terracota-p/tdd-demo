aa3c13e3bed8011e172d3f6d9281c8c9e817d257
c.friaszapater <c.friaszapater@orwellg.com>
2019-06-16 10:46:00 +0200

    spring-boot maven project

---
+++ src/main/java/tdddemo/TddDemoApplication.java
@@ -0,0 +1,13 @@
+package tdddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDemoApplication.class, args);
+	}
+
+}
