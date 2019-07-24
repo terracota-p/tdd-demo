f54ccfe4560a24c17366e56218aa677429a395f2
c.friaszapater <c.friaszapater@orwellg.com>
2019-07-24 14:04:12 +0200

    green health check

--- acceptance-tests/features/support/init.js
+++ acceptance-tests/features/support/init.js
@@ -4,6 +4,6 @@ const apickli = require('apickli');
 const {Before} = require('cucumber');
 
 Before(function() {
-    this.apickli = new apickli.Apickli('http', 'httpbin.org');
+    this.apickli = new apickli.Apickli('http', 'localhost:8080');
     this.apickli.addRequestHeader('Cache-Control', 'no-cache');
 });
