bd4c66c5007004f682bd6db3f98254a5615b9d36
c.friaszapater <c.friaszapater@orwellg.com>
2019-07-24 09:29:07 +0200

    application integration test skeleton

--- src/test/java/tdddemo/TddDemoApplicationTests.java
+++ src/test/java/tdddemo/TddDemoApplicationTests.java
@@ -2,15 +2,33 @@ package tdddemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class TddDemoApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+    
 	@Test
-	public void contextLoads() {
+	public void shouldSaveCharacterWithName() {
+	    // TODO implement
 	}
+
+    @Test
+    public void shouldFailOnSaveCharacterWithoutName() {
+        // TODO implement
+    }
+
+    @Test
+    public void shouldRetrieveSavedCharacterByName() {
+        // TODO implement
+    }
 
 }
