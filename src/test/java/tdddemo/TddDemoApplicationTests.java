cfa1c7d6bdadf1962cfde00baee2d6a8924dc017
c.friaszapater <c.friaszapater@orwellg.com>
2019-07-25 08:49:49 +0200

    add Scenario: 1.2. Error saving character - No name provided - RED TddDemoApplicationTests

--- src/test/java/tdddemo/TddDemoApplicationTests.java
+++ src/test/java/tdddemo/TddDemoApplicationTests.java
@@ -1,12 +1,18 @@
 package tdddemo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,15 +21,21 @@ public class TddDemoApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
-    
-	@Test
-	public void shouldSaveCharacterWithName() {
-	    // TODO implement
-	}
 
     @Test
-    public void shouldFailOnSaveCharacterWithoutName() {
+    public void shouldSaveCharacterWithName() {
         // TODO implement
+    }
+
+    @Test
+    public void shouldFailOnSaveCharacterWithoutName() throws Exception {
+        // Given I set body to {"leadership":3, "endurance":2}
+        // When I POST to /characters
+        MockHttpServletRequestBuilder request = post("http://localhost/characters").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"leadership\":3, \"endurance\":2}");
+
+        // Then response code should be 400
+        mockMvc.perform(request).andDo(print()).andExpect(status().isBadRequest());
     }
 
     @Test
